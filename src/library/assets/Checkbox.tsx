d7cc6d4677007875e037ddb66c8f4c89999c4714
PintoDev64 <cardozojoan13@gmail.com>
2024-11-05 18:53:56 -0500

    Fix clipPath Propertie

--- src/library/assets/Checkbox.tsx
+++ src/library/assets/Checkbox.tsx
@@ -1,5 +1,5 @@
 export default function CheckboxSVG() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13" width="13pt" height="13pt"><defs><clipPath id="_clipPath_8yux0LdQxLK0wbCLq7MR3yfh7eKebhPk"><rect width="13" height="13" /></clipPath></defs><g clip-path="url(#_clipPath_8yux0LdQxLK0wbCLq7MR3yfh7eKebhPk)"><rect width="13" height="13" style={{ fill: "rgb(0,0,0)" }} fill-opacity="0" /><path d=" M 10.563 1.698 L 5.069 7.199 L 2.437 4.571 L 0.385 6.626 L 5.073 11.302 L 12.615 3.745 L 10.563 1.698 Z " fill="rgb(255,255,255)" /></g></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13" width="13pt" height="13pt"><defs><clipPath id="_clipPath_8yux0LdQxLK0wbCLq7MR3yfh7eKebhPk"><rect width="13" height="13" /></clipPath></defs><g clipPath="url(#_clipPath_8yux0LdQxLK0wbCLq7MR3yfh7eKebhPk)"><rect width="13" height="13" style={{ fill: "rgb(0,0,0)" }} fill-opacity="0" /><path d=" M 10.563 1.698 L 5.069 7.199 L 2.437 4.571 L 0.385 6.626 L 5.073 11.302 L 12.615 3.745 L 10.563 1.698 Z " fill="rgb(255,255,255)" /></g></svg>
     )
 }
